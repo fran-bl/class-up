c7192dd038d1bbfcf96b7bdaf7eaee3de9aefa0c
Fran Blažević <fran.blazevic0@gmail.com>
2025-05-26 01:10:37 +0200

    Refined challenge leaderboard screen

--- src/app/challenge/[id]/page.tsx
+++ src/app/challenge/[id]/page.tsx
@@ -1,6 +1,7 @@
-import { getChallengeById, getParticipants } from "@/app/actions";
+import { getChallengeById, getFormattedDate, getParticipants } from "@/app/actions";
 import ChallengeLeaderboard from "@/components/challenge-leaderboard";
 import RoleGate from "@/components/role-gate";
+import { Progress } from "@/components/ui/progress";
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
 
@@ -17,13 +18,24 @@ export default async function ChallengePage({ params }) {
     const challenge = await getChallengeById(id);
     const participants = await getParticipants(id);
 
+    const start = await getFormattedDate(challenge.start_date);
+    const end = await getFormattedDate(challenge.end_date);
+
     return (
         <RoleGate allowedRoles={["student", "admin"]}>
-            <div className="container mx-auto p-4">
-                <h1 className="text-2xl font-bold mb-4">{challenge.title}</h1>
-                <p className="mb-4">{challenge.description}</p>
-                <ChallengeLeaderboard participants={participants} />
+            <div className="flex flex-col items-center justify-center p-4">
+                <h1 className="text-4xl font-bold mb-4 max-sm:text-2xl max-sm:mt-16">{challenge.title}</h1>
+                <p className="mb-4 text-xl max-sm:text-lg">{challenge.description}</p>
+                <div>Reward: <span className="text-2xl font-bold text-green-500">{challenge.xp_reward}</span> XP</div>
+                <p className="text-lg text-gray-500 max-sm:text-sm">
+                    {start} - {end}
+                </p>
+                <Progress value={challenge.progress / challenge.target_value * 100} indicatorColor="bg-green-500" className="w-1/2 mt-10 max-sm:w-3/4" />
+                <p className="text-lg text-gray-500 mt-2">
+                    Progress: {challenge.progress} / {challenge.target_value}
+                </p>
             </div>
+            <ChallengeLeaderboard participants={participants} />
         </RoleGate>
     );
 }
--- src/components/challenge-leaderboard.tsx
+++ src/components/challenge-leaderboard.tsx
@@ -21,30 +21,79 @@ export default function ChallengeLeaderboard({ participants }: { participants: C
             level: getLevel(participant.profile.xp)[0],
         }));
 
+    if (sortedParticipants.length === 0) {
+        return (
+            <p className="text-center text-lg text-gray-500 mt-10">
+                No participants have contributed yet. Be the first to contribute!
+            </p>
+        );
+    }
+
     return (
-        <div className="flex flex-col items-center justify-center p-5">
-            {sortedParticipants.map((participant, index) => (
-                <div key={participant.profile_id} className="flex items-center justify-between w-full max-w-md mb-2rounded-lg">
-                    <div className="grid grid-cols-3 items-center gap-4 w-1/2">
-                        <span className="text-3xl">{index + 1}.</span>
-                        <div className='relative w-12 h-14 -ml-5'>
-                            <Avatar className='w-10 h-10 absolute top-2 left-[3]'>
-                                {participant.profile.avatar_url && <AvatarImage src={participant.profile.avatar_url} />}
-                                <AvatarFallback>{participant.profile.username[0]}</AvatarFallback>
-                            </Avatar>
-                            <Image
-                                src={`/images/levels/lvl${participant.level < 9 ? participant.level : "endgame"}.png`}
-                                alt={`Level ${participant.level} border`}
-                                width={48}
-                                height={56}
-                                className="absolute inset-0 pointer-events-none object-contain"
-                            />
+        <>
+            <div className="relative flex flex-col items-center justify-center mt-[15%] max-sm:mt-[50%]">
+                <Image
+                    src="/images/podium.webp"
+                    alt="Podium image"
+                    width={500}
+                    height={500}
+                    className="object-cover mb-4"
+                />
+                {sortedParticipants.slice(0, 3).map((participant, idx) => {
+                    const positions = [
+                        "left-[53%] -translate-x-1/2 -top-[50%] max-sm:left-[62%] max-sm:-top-[75%]",
+                        "left-[38%] -top-[25%] max-sm:left-[10%] max-sm:-top-[50%]",
+                        "left-[57%] -top-[17%] max-sm:left-[67%] max-sm:-top-[42%]",
+                    ];
+                    return (
+                        <div
+                            key={participant.profile_id}
+                            className={`absolute flex flex-col items-center ${positions[idx]} z-10`}
+                            style={{ transform: idx === 0 ? "translate(-50%, 0)" : undefined }}
+                        >
+                            <div className="relative w-20 h-20 left-2">
+                                <Avatar className="w-15 h-15 top-3 left-[4]">
+                                    {participant.profile.avatar_url && <AvatarImage src={participant.profile.avatar_url} />}
+                                    <AvatarFallback>{participant.profile.username[0]}</AvatarFallback>
+                                </Avatar>
+                                <Image
+                                    src={`/images/levels/lvl${participant.level < 9 ? participant.level : "endgame"}.png`}
+                                    alt={`Level ${participant.level} border`}
+                                    width={70}
+                                    height={82}
+                                    className="absolute inset-0 pointer-events-none object-contain"
+                                />
+                            </div>
+                            <span className="text-lg font-bold">{participant.profile.username}</span>
+                            <span className="text-md">{participant.contribution} pts</span>
                         </div>
-                        <span className="text-lg sm:-ml-5">{participant.profile.username}</span>
+                    );
+                })}
+            </div>
+            <div className="flex flex-col items-center justify-center p-5">
+                {sortedParticipants.slice(3).map((participant, index) => (
+                    <div key={participant.profile_id} className="flex items-center justify-between w-full max-w-md mb-2rounded-lg">
+                        <div className="grid grid-cols-3 items-center gap-4 w-1/2">
+                            <span className="text-3xl">{index + 4}.</span>
+                            <div className='relative w-12 h-14 -ml-5'>
+                                <Avatar className='w-10 h-10 absolute top-2 left-[3]'>
+                                    {participant.profile.avatar_url && <AvatarImage src={participant.profile.avatar_url} />}
+                                    <AvatarFallback>{participant.profile.username[0]}</AvatarFallback>
+                                </Avatar>
+                                <Image
+                                    src={`/images/levels/lvl${participant.level < 9 ? participant.level : "endgame"}.png`}
+                                    alt={`Level ${participant.level} border`}
+                                    width={48}
+                                    height={56}
+                                    className="absolute inset-0 pointer-events-none object-contain"
+                                />
+                            </div>
+                            <span className="text-lg sm:-ml-5">{participant.profile.username}</span>
+                        </div>
+                        <span className="text-lg font-bold">{participant.contribution} points</span>
                     </div>
-                    <span className="text-lg font-bold">{participant.contribution} points</span>
-                </div>
-            ))}
-        </div>
+                ))}
+            </div>
+        </>
     );
 }
